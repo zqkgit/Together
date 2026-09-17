40e1325f552d7b1b73be020a79d068c25c19ae46
周钦凯 <1290909715@qq.com>
2026-09-17 18:59:03 +0800

    update

--- TogetherCode/Together_api/src/routes/adminStudent.js
+++ TogetherCode/Together_api/src/routes/adminStudent.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const { validateRequest } = require("../middlewares/validate");
 const { listStudioStudentsValidators, consumeLessonValidators } = require("../validators/studentValidator");
-const { getStudioStudents, postConsumeLessons } = require("../controllers/studentController");
+const { getStudioStudents, postConsumeLessons, getStudentLessonLogs } = require("../controllers/studentController");
 
 const router = express.Router();
 
 // 角色归属：工作室后台（Web）。
-// 已实现接口：学员列表、后台手动消课。
+// 已实现接口：学员列表、课时流水、后台手动消课。
 router.get("/", listStudioStudentsValidators, validateRequest, getStudioStudents);
+router.get("/:id/logs", getStudentLessonLogs);
 router.post("/:id/consume", consumeLessonValidators, validateRequest, postConsumeLessons);
 
 module.exports = router;
